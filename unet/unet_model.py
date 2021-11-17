ea462698aa897df00841c9029a6e5f949a9e2fc3
snusun <0515hwang@snu.ac.kr>
2021-11-17 20:59:04 +0900

    fusionNet technique applied

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -16,10 +16,17 @@ class UNet(nn.Module):
         self.down3 = Down(256, 512)
         factor = 2 if bilinear else 1
         self.down4 = Down(512, 1024 // factor)
-        self.up1 = Up(1024, 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
+
+
+        self.up1 = Up(512, 512 // factor, bilinear)
+        self.up2 = Up(256, 256 // factor, bilinear)
+        self.up3 = Up(128, 128 // factor, bilinear)
+        self.up4 = Up(64, 64, bilinear)
+
+        #self.up1 = Up(1024, 512 // factor, bilinear)
+        #self.up2 = Up(512, 256 // factor, bilinear)
+        #self.up3 = Up(256, 128 // factor, bilinear)
+        #self.up4 = Up(128, 64, bilinear)
         self.outc = OutConv(64, n_classes)
 
     def forward(self, x):
@@ -28,9 +35,33 @@ class UNet(nn.Module):
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
+
+        print("x5 x4 shape")
+        print(x5.shape, x4.shape)
+        
         x = self.up1(x5, x4)
+
+        print("x x3 shape")
+        print(x.shape, x3.shape)
+
         x = self.up2(x, x3)
+
+        print("x x2 shape")
+        print(x.shape, x2.shape)
+
         x = self.up3(x, x2)
+
+        print("x x1 shape")
+        print(x.shape, x1.shape)
+
         x = self.up4(x, x1)
+
+        print("x shape")
+        print(x.shape)
+
         logits = self.outc(x)
+
+        print("logits shape")
+        print(logits.shape)
+
         return logits
--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -5,6 +5,42 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+def conv_block(in_channels, out_channels):
+    conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True))
+    return conv
+
+def conv_block_3(in_channels, out_channels):
+    conv = nn.Sequential(
+            conv_block(in_channels, out_channels),
+            conv_block(in_channels, out_channels),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True))
+    return conv
+
+class Conv_residual_conv(nn.Module):
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+
+        self.conv_1 = conv_block(self.in_channels,self.out_channels)
+        self.conv_2 = conv_block_3(self.out_channels,self.out_channels)
+        self.conv_3 = conv_block(self.out_channels,self.out_channels)
+
+    def forward(self,input):
+        conv_1 = self.conv_1(input)
+        conv_2 = self.conv_2(conv_1)
+        res = conv_1 + conv_2
+        conv_3 = self.conv_3(res)
+        return conv_3
+
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
@@ -32,7 +68,8 @@ class Down(nn.Module):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
             nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels)
+            Conv_residual_conv(in_channels, out_channels)
+            #DoubleConv(in_channels, out_channels)
         )
 
     def forward(self, x):
@@ -44,14 +81,15 @@ class Up(nn.Module):
 
     def __init__(self, in_channels, out_channels, bilinear=True):
         super().__init__()
-
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+            self.conv = Conv_residual_conv(in_channels, out_channels)
+            #self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
         else:
             self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
+            self.conv = Conv_residual_conv(in_channels, out_channels)
+            #self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
@@ -61,10 +99,14 @@ class Up(nn.Module):
 
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
                         diffY // 2, diffY - diffY // 2])
+
         # if you have padding issues, see
         # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-        x = torch.cat([x2, x1], dim=1)
+        #print("x1 x2 shape")
+        #print(x1.shape, x2.shape)
+        x = (x1 + x2)/2 
+        #x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
 
 
