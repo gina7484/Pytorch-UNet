1a0bbd09b3c085a75006cbd8390273f2c7adba59
Gina Sohn <gina7484@gmail.com>
2021-11-23 02:08:02 +0900

    add augmentation dataset

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import wandb
 from torch import optim
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader, random_split, ConcatDataset
 from tqdm import tqdm
 
 from utils.data_loading import BasicDataset, CarvanaDataset
@@ -20,8 +20,8 @@ from unet import UNet
 
 dir_img = Path('../2D/training/image/')
 dir_mask = Path('../2D/training/label/')
-#dir_img = Path('./data/imgs/')
-#dir_mask = Path('./data/masks/')
+dir_aug_img = Path('./data/imgs/')
+dir_aug_mask = Path('./data/masks/')
 checkpoint_parent="./checkpoints/"
 
 
@@ -39,15 +39,17 @@ def train_net(net,
         dataset = CarvanaDataset(dir_img, dir_mask, img_scale)
     except (AssertionError, RuntimeError):
         dataset = BasicDataset(dir_img, dir_mask, img_scale)
+        augDataset = BasicDataset(dir_aug_img, dir_aug_mask, img_scale)
 
     # 2. Split into train / validation partitions
     n_val = int(len(dataset) * val_percent)
     n_train = len(dataset) - n_val
     train_set, val_set = random_split(dataset, [n_train, n_val], generator=torch.Generator().manual_seed(0))
+    concated_set = ConcatDataset([train_set,augDataset])
 
     # 3. Create data loaders
     loader_args = dict(batch_size=batch_size, num_workers=4, pin_memory=True)
-    train_loader = DataLoader(train_set, shuffle=True, **loader_args)
+    train_loader = DataLoader(concated_set, shuffle=True, **loader_args)
     val_loader = DataLoader(val_set, shuffle=False, drop_last=True, **loader_args)
 
     # (Initialize logging)
